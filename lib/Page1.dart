1aea476e226c580411758ad9e45486061be3a3d4
USER <srrajib61@gmail.com>
2021-11-01 15:33:43 +0600

    Initial commit

---
+++ lib/Page1.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatefulWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     home: MyPage(),
+    );
+  }
+}
+class MyPage extends StatefulWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+class _MyPageState extends State<MyPage> {
+  int _re=0;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+        actions: <Widget>[
+          IconButton(onPressed: (){
+            setState(() {
+              _re=0;
+            });
+          }, icon: Icon(Icons.refresh))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+
+             Text('$_re',style: TextStyle(fontSize: 50),),
+          ],
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        setState(() {
+          _re++;
+        });
+      },
+      child: Icon(Icons.add),
+      ),
+
+    );
+  }
+}
+
